415c824683816a01812055687b0683102ac1eb1c
Dima <dimazuh@gmail.com>
2023-11-25 19:59:09 +0200

    added: CORS

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from auth import auth_routes
 from routes import user_routes, post_routes, comment_routes
@@ -14,11 +15,15 @@ app.include_router(post_routes.router)
 app.include_router(auth_routes.router)
 app.include_router(comment_routes.router)
 
+origins = ['http://localhost:3000']
 
-@app.get('/')
-def root():
-    return 'Hello world!'
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 models.Base.metadata.create_all(engine)
 
